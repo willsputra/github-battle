b9a3117d6bd2d31c9aed777e55211254afaaf856
William Suryadiputra Goi <will_s_putra@yahoo.com>
2016-10-16 14:37:58 +0800

    video4_fixed

--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
-var USER_DATA = {
-  name: 'WilliamX',
-  username: 'willsputra',
-  image: 'http://sydney.edu.au/sca/news/2010/images/test_site_2.jpg'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height:100, width:100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function(){
-    return(
-      <div>{this.props.name}</div>
-    );
-  }
-});
-
-var Avatar = React.createClass({
-  render: function(){
-    return (
-      <div>
-          <ProfilePic imageUrl={this.props.user.image} />
-          <ProfileName name={this.props.user.name} />
-          <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>,
+  routes,
   document.getElementById('app')
 );
