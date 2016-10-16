49b6fb8915357b0b94a16c3fedadc9e8fcb13133
William Suryadiputra Goi <will_s_putra@yahoo.com>
2016-10-16 13:57:26 +0800

    video-purefunctions

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: 'WilliamX',
+  username: 'willsputra',
+  image: 'http://sydney.edu.au/sca/news/2010/images/test_site_2.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function (){
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height:100, width:100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
     return (
-      <div>Hello Reactjs Program</div>
-    )
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return(
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+          <ProfilePic imageUrl={this.props.user.image} />
+          <ProfileName name={this.props.user.name} />
+          <ProfileLink username={this.props.user.username} />
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
