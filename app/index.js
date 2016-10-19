9c10233792c4eb2a704cc70ab27c8ab3805fd8d6
William Suryadiputra Goi <will_s_putra@yahoo.com>
2016-10-19 09:15:02 +0800

    video4_fixed2

--- app/index.js
+++ app/index.js
@@ -1,6 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
+var Raven = require('raven-js')
+
+var sentryKey = 'f63fd193847840e69953983870798648';
+var sentryApp = '106482';
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch,
+  }
+}).install()
 
 ReactDOM.render(
   routes,
