ab2cb1c49b4d9b84465050d6427d05b55166a595
William Suryadiputra Goi <will_s_putra@yahoo.com>
2016-10-25 18:57:27 +0800

    video5_final

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
+function ConfirmBattle (props) {
+  return props.isLoading === true
+    ? <p> LOADING! </p>
+    : <div> CONFIRM BATTLE: {puke(props)} </div>
+
+}
+
+module.exports = ConfirmBattle;
